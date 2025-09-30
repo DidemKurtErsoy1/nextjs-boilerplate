77ea4da8ee578ae8035e3e4b57c5ee9ed3f35d25
didemkurtersoy-sudo <didemkurtersoy@gmail.com>
2025-09-30 09:44:44 +0300

    Create sw-register.tsx

---
+++ app/sw-register.tsx
@@ -0,0 +1,13 @@
+// app/sw-register.tsx
+'use client';
+
+import { useEffect } from 'react';
+
+export default function SwRegister() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {});
+    }
+  }, []);
+  return null;
+}
